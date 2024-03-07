b78decf2b956695ffe3b170787172e13816beac6
ophirshiran <ophir.shiran@post.runi.ac.il>
2024-03-07 08:30:26 +0200

    question1

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -3,8 +3,9 @@
 public class HashTagTokenizer {
 
 	public static void main(String[] args) {
-
-		String hashTag = args[0];
+	
+		String hashTag = "#iloveUNIVERSITY";
+		//String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
 	}
@@ -14,13 +15,21 @@ public class HashTagTokenizer {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for(int i=0; i<dictionary.length; i++){
+			dictionary[i]=in.readLine();
+		}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
 		// Your code here
+		for(int i=0; i<dictionary.length; i++){
+			if(dictionary[i].equals(word)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -31,10 +40,27 @@ public class HashTagTokenizer {
         }
  
         int N = hashtag.length();
+		
+		hashtag=hashtag.toLowerCase();
 
         for (int i = 1; i <= N; i++) {
+			if(hashtag.substring(0,1).charAt(0)=='#'){
+				breakHashTag(hashtag.substring(1), dictionary);
+				break;
+			}
+			else	if(existInDictionary(hashtag.substring(0,i), dictionary)==true){
+				System.out.println(hashtag.substring(0,i));
+				breakHashTag(hashtag.substring(i,N), dictionary);
+				break;
+			}
+			
 		
         }
+
+	
+
+	
+		
     }
 
 }
